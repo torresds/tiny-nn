1e8dd509fc14e8c7d2865cddb4e4f44e79682115
Miguel <migueltrrds@gmail.com>
2026-01-14 19:08:14 -0300

    chore: minor fixes and example/build adjustments

--- examples/classify_blobs.cpp
+++ examples/classify_blobs.cpp
@@ -11,35 +11,29 @@
 using namespace tf;
 
 int main() {
-  
+
   int samples = 1000;
   int features = 2;
   int classes = 3;
   float cluster_std = 1.0f;
   std::cout << "Generating " << samples << " blobs..." << std::endl;
 
-  
   auto dataset = make_blobs(samples, features, classes, cluster_std, 42);
 
-  
   DataLoader loader(dataset, 32, true, 42);
 
-  
   RNG rng(42);
   Sequential model;
 
-  
   model.add(new Dense(features, 16, rng));
   model.add(new ReLU());
   model.add(new Dense(16, classes, rng));
 
-  
   Adam optim(0.01f);
   auto params = model.params();
 
   std::cout << "Model params: " << params.size() << std::endl;
 
-  
   int epochs = 20;
   for (int epoch = 0; epoch < epochs; ++epoch) {
     float epoch_loss = 0.0f;
@@ -49,25 +43,18 @@ int main() {
 
     Tensor X, Y;
     while (loader.next(X, Y)) {
-      
+
       optim.zero_grad(params);
 
-      
       Tensor logits = model.forward(X);
 
-      
       Tensor d_logits;
       float loss = softmax_cross_entropy_with_logits(logits, Y, d_logits);
       epoch_loss += loss;
       batches++;
 
-      
-      
-
-      
       model.backward(d_logits);
 
-      
       optim.step(params);
     }
     loader.reset();
--- src/optim/sgd.cpp
+++ src/optim/sgd.cpp
@@ -2,18 +2,19 @@
 
 namespace tf {
 
-void SGD::zero_grad(const std::vector<Param>& ps) {
-  for (auto& p : ps) {
-    p.grad->fill_(0.0f);
+void SGD::zero_grad(const std::vector<Param> &ps) {
+  for (auto &p : ps) {
+    if (p.grad)
+      p.grad->fill_(0.0f);
   }
 }
 
-void SGD::step(const std::vector<Param>& ps) {
-  for (auto& p : ps) {
+void SGD::step(const std::vector<Param> &ps) {
+  for (auto &p : ps) {
     for (size_t i = 0; i < p.value->size(); ++i) {
       p.value->data[i] -= lr_ * p.grad->data[i];
     }
   }
 }
 
-}
+}  
